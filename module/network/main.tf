f678986aa9fafdbd011710c1805c51c755512493
ashwini9860 <auchoudhari@gmail.com>
2019-10-01 16:25:11 +0530

    Update main.tf

--- module/network/main.tf
+++ module/network/main.tf
@@ -75,6 +75,13 @@ resource "aws_security_group" "demo" {
   }
   
   ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
     from_port = 6443
     to_port = 6443
     protocol = "tcp"
